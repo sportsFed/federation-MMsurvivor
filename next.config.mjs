70f74af57a06e674d26796787cb26c6ae80bac8d
The Sports Fed <thesportsfederation@gmail.com>
2026-03-13 16:17:14 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Any specific config options go here
+  transpilePackages: ['undici', 'firebase'],
+  // This helps Next.js ignore the modern JS syntax it doesn't understand yet
+  experimental: {
+    serverComponentsExternalPackages: ['firebase-admin'],
+  },
 };
 
 export default nextConfig;
