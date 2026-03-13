068cbc6f73c9e99456c8213ef699fa7d02c93169
The Sports Fed <thesportsfederation@gmail.com>
2026-03-13 16:44:57 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,12 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        bebas: ['var(--font-bebas)'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
